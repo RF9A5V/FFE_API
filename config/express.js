562b5c9e216ed7bd1a40849612e720e5654a983e
Steven Chen <steven@thecrudeintellectual.com>
2015-11-22 00:30:50 -0800

    Fixed CORS

--- config/express.js
+++ config/express.js
@@ -30,8 +30,10 @@ module.exports = function(app, config, mongoose) {
   app.use(methodOverride());
 
   var enableCORS = function(req, res, next){
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Origin', req.headers.origin || "*");
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,HEAD,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'content-Type,x-requested-with');
+    res.header('Access-Control-Allow-Credentials', 'true')
     next();
   }
 
